b9d59d3e6811485727c2ee197f26993c0cfad54a
ghcdn <chenyulin@yandex.com>
2022-05-24 15:07:37 +0800

    typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@ VERSION=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowso
 SS_URL="https://github.com/shadowsocks/shadowsocks-rust/releases/download/${VERSION}/shadowsocks-${VERSION}.x86_64-unknown-linux-musl.tar.xz"
 wget ${SS_URL} && tar xf *-linux-musl.tar.xz -C /usr/local/bin && rm *-linux-musl.tar.xz
 wget https://dl.lamp.sh/files/v2ray-plugin_linux_amd64 -O /usr/local/bin/v2-plugin
-chmod a+x /usr/local/bin/ss-server /usr/local/bin/v2-plugin
+chmod a+x /usr/local/bin/ssserver /usr/local/bin/v2-plugin
 
-ss-server -s 0.0.0.0 -p ${PORT} -k ${PASSWORD} -m $METHOD --plugin v2-plugin --plugin-opts "server"
+ssserver -s 0.0.0.0 -p ${PORT} -k ${PASSWORD} -m $METHOD --plugin v2-plugin --plugin-opts "server"
