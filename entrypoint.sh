82bd76ba5a78708e37e627d1b7d6958d1886ac9f
ghcdn <chenyulin@yandex.com>
2022-05-24 14:59:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 VERSION=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | grep 'tag_name' | cut -d\" -f4)
 SS_URL="https://github.com/shadowsocks/shadowsocks-rust/releases/download/${VERSION}/shadowsocks-${VERSION}.x86_64-unknown-linux-musl.tar.xz"
-wget -c ${SS_URL} -O - | tar xzC /usr/local/bin
+wget ${SS_URL} && tar xf *-linux-musl.tar.xz -C /usr/local/bin && rm *-linux-musl.tar.xz
 wget https://dl.lamp.sh/files/v2ray-plugin_linux_amd64 -O /usr/local/bin/v2-plugin
 chmod a+x /usr/local/bin/ss-server /usr/local/bin/v2-plugin
 
