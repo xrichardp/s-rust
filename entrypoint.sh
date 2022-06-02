37f6fce83bb432f045713e79047be77f03692a23
ghcdn <48314862+YulinChan@users.noreply.github.com>
2022-06-02 23:56:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 plugin='v''2r''ay-p''lugin'
 wget https://dl.lamp.sh/files/${plugin}_linux_amd64 -O /usr/local/bin/${plugin}
 chmod +x /usr/local/bin/${plugin}
-## start service
-ssserver -s "0.0.0.0:${PORT}" -m "chacha20-ietf-poly1305" -k "${PASSWORD}" --plugin "${plugin}" --plugin-opts "server"
+ssserver -s "[::]:${PORT}" -m "chacha20-ietf-poly1305" -k "${PASSWORD}" --plugin "${plugin}" --plugin-opts "server"
