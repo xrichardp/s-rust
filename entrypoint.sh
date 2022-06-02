74ceb61273ca88bef569592cc4a97c7b741e24c6
ghcdn <48314862+YulinChan@users.noreply.github.com>
2022-06-03 00:00:10 +0800

    Revert "Update entrypoint.sh"
    This reverts commit 37f6fce83bb432f045713e79047be77f03692a23.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 plugin='v''2r''ay-p''lugin'
 wget https://dl.lamp.sh/files/${plugin}_linux_amd64 -O /usr/local/bin/${plugin}
 chmod +x /usr/local/bin/${plugin}
-ssserver -s "[::]:${PORT}" -m "chacha20-ietf-poly1305" -k "${PASSWORD}" --plugin "${plugin}" --plugin-opts "server"
+## start service
+ssserver -s "0.0.0.0:${PORT}" -m "chacha20-ietf-poly1305" -k "${PASSWORD}" --plugin "${plugin}" --plugin-opts "server"
