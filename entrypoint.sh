e060709029bf72e68808a701b356a89cf2a5667e
ghcdn <48314862+YulinChan@users.noreply.github.com>
2022-06-10 17:31:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 ## install plugin
-wget https://github.com/maskedeken/gost-plugin/releases/download/v1.6.1/gost-plugin_linux_amd64-1.6.1.zip -qO plugin.zip
-unzip plugin.zip -oqd /usr/local/bin
+url=$(wget -qO- https://api.github.com/repos/maskedeken/gost-plugin/releases/latest | grep -Eo 'https.*?linux-amd64.*?gz')
+wget -q $url && tar xf *.tar.gz && mv linux-amd64/gost-plugin usr/local/bin && rm -rf *linux-amd64*
 chmod a+x /usr/local/bin/gost-plugin
 ## start service
-ssserver -s "0.0.0.0:${PORT}" -m "chacha20-ietf-poly1305" -k "${PASSWORD}" --plugin gost-plugin --plugin-opts "server;mode=ws"
+ssserver -s "0.0.0.0:${PORT}" -m "aes-256-gcm" -k "${PASSWORD}" --plugin gost-plugin --plugin-opts "server;mode=ws"
