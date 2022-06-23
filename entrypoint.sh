ed831212da6218b120bb6ba2ed35c425f9fc1deb
ghcdn <cylind@proton.me>
2022-06-23 13:24:03 +0800

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,27 @@
-## install plugin
-plugin='v''2r''ay-p''lugin'
-wget https://dl.lamp.sh/files/${plugin}_linux_amd64 -O /usr/local/bin/${plugin}
-chmod +x /usr/local/bin/${plugin}
+## install cloak
+url='https://github.com/cbeuw/Cloak/releases/download/v2.6.0/ck-server-linux-amd64-v2.6.0'
+wget $url -O /usr/local/bin/ck-server
+chmod +x /usr/local/bin/ck-server
+## setup cloak
+cat << EOF > /opt/server.json
+{
+  "ProxyBook": {
+    "shadowsocks": [
+      "tcp",
+      "127.0.0.1:9008"
+    ]
+  },
+  "BindAddr": [
+    ":${PORT}"
+  ],
+  "BypassUID": [
+    "${UID}"
+  ],
+  "RedirAddr": "cloudflare.com",
+  "PrivateKey": "${KEY}",
+  "AdminUID": "${UID}",
+  "DatabasePath": "userinfo.db"
+}
+EOF
 ## start service
-ssserver -s "0.0.0.0:${PORT}" -m "aes-256-gcm" -k "${PASSWORD}" --plugin "${plugin}" --plugin-opts "server"
+ssserver -s "0.0.0.0:9008" -m "aes-256-gcm" -k "${PASSWORD}" --plugin "ck-server" --plugin-opts "/opt/server.json"
